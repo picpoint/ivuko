6943fb15fe36cdf3a1087ecf91bb2ca2fbab8698
rmtar <picpoint@yandex.ru>
2021-08-24 08:52:59 +0300

    make partition in admin panel

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -170,17 +170,6 @@
                 </div>
             </div>
 
-            <!-- SidebarSearch Form -->
-            {{--<div class="form-inline">--}}
-                {{--<div class="input-group" data-widget="sidebar-search">--}}
-                    {{--<input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">--}}
-                    {{--<div class="input-group-append">--}}
-                        {{--<button class="btn btn-sidebar">--}}
-                            {{--<i class="fas fa-search fa-fw"></i>--}}
-                        {{--</button>--}}
-                    {{--</div>--}}
-                {{--</div><div class="sidebar-search-results"><div class="list-group"><a href="#" class="list-group-item"><div class="search-title"><strong class="text-light"></strong>N<strong class="text-light"></strong>o<strong class="text-light"></strong> <strong class="text-light"></strong>e<strong class="text-light"></strong>l<strong class="text-light"></strong>e<strong class="text-light"></strong>m<strong class="text-light"></strong>e<strong class="text-light"></strong>n<strong class="text-light"></strong>t<strong class="text-light"></strong> <strong class="text-light"></strong>f<strong class="text-light"></strong>o<strong class="text-light"></strong>u<strong class="text-light"></strong>n<strong class="text-light"></strong>d<strong class="text-light"></strong>!<strong class="text-light"></strong></div><div class="search-path"></div></a></div></div>--}}
-            {{--</div>--}}
 
             <!-- Sidebar Menu -->
             <nav class="mt-2">
@@ -209,6 +198,90 @@
                                 <a href="../../index3.html" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Удалить изделие</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fab fa-buffer"></i>
+                            <p>Категории
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="../../index.html" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Создать категорию</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="../../index2.html" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Редактировать категорию</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="../../index3.html" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Удалить категорию</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fab fa-medium"></i>
+                            <p>Материал
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="../../index.html" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Создать материал</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="../../index2.html" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Редактировать материал</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="../../index3.html" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Удалить материал</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-cubes"></i>
+                            <p>Вставки
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="../../index.html" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Создать вставку</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="../../index2.html" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Редактировать вставку</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="../../index3.html" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Удалить вставку</p>
                                 </a>
                             </li>
                         </ul>
